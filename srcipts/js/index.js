f834bbebb7202ae66a26e8a432d5d2265ef036bc
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-08-17 18:25:51 +1000

    Update

--- srcipts/js/index.js
+++ srcipts/js/index.js
@@ -6,10 +6,20 @@
         #height = 10
 
         #grid = []
-        #elmGrid
+        #elmGrid = []
+        gridElm
+
+        #crntPath = []
+        #crntNabours = []
+        #pathStarted = false
+        #crntElm
+        #crntX
+        #crntY
 
         #number = 0
         #factors = []
+
+        #nabourListner
 
         constructor(width, height) {
             this.#width = width ?? 10
@@ -19,6 +29,8 @@
             this.#factors = n.f
 
             this.#gengrid()
+            this.genElmGrid()
+            this.#nabourListner = this.#nabourListnerN.bind(this)
         }
 
         #gengrid() {
@@ -40,11 +52,110 @@
             console.log(this.#grid)
         }
 
-        genGrid() {
-            for (let x = 0; x < array.length; x++) {
-                for (let y = 0; y < array.length; y++) {
+        #minMax(min, x, max) {
+            return Math.min(Math.max(x, min), max)
+        }
+
+        getNabouringCells(dx, dy) {
+            console.log(dx, dy)
+            var cells = []
+            for (
+                let x = Math.max(dx - 1, 0);
+                x < Math.min(dx + 2, this.#width);
+                x++
+            ) {
+                for (
+                    let y = Math.max(dy - 1, 0);
+                    y < Math.min(dy + 2, this.#height);
+                    y++
+                ) {
+                    if ((x == dx) & (y == dy)) continue;
+                    cells.push({
+                        x: x,
+                        y: y,
+                        cell: this.#elmGrid[x]?.[y]
+                    })
                 }
             }
+            return cells
+        }
+
+        beginPath(elm, x, y) {
+            this.#crntX = x
+            this.#crntY = y
+            this.#crntElm = elm
+            this.#pathStarted = true
+            this.#crntPath.push(elm)
+            this.#crntNabours = this.getNabouringCells(x, y)
+            this.#crntNabours.forEach(t => {
+                t.cell.style.background = 'yellow'
+                t.cell.addEventListener('mousedown', this.#nabourListner)
+            })
+        }
+
+        addPoint(elm, x, y) {
+            console.log(this.#crntPath)
+            this.#crntPath.push(this.#crntElm)
+            this.#crntElm.style.background = 'green'
+            this.#crntElm = elm
+            this.#crntX = x
+            this.#crntY = y
+            console.log(this.#crntNabours.map(r => r.cell))
+            this.#crntNabours.forEach(t => {
+                t.cell.style.background = ''
+                t.cell.removeEventListener('mousedown', this.#nabourListner)
+            })
+            this.#crntNabours = this.getNabouringCells(x, y).filter(t => !this.#crntPath.includes(t.cell))
+            this.#crntNabours.forEach(t => {
+                t.cell.style.background = 'yellow'
+                t.cell.addEventListener('mousedown', this.#nabourListner)
+            })
+        }
+
+        endPath() {
+
+        }
+
+        /**
+         * @param {MouseEvent} e 
+         */
+        #nabourListnerN(e) {
+            this.addPoint(
+                e.target,
+                e.target.getAttribute('x'),
+                e.target.getAttribute('y')
+            )
+        }
+
+        /**
+         * 
+         * @param {HTMLTableCellElement} elm 
+         * @param {number} x 
+         * @param {number} y 
+         */
+        #provideFunctionality(elm, x, y) {
+            elm.addEventListener('mousedown', (() => {
+                if (this.#pathStarted) return;
+                this.beginPath(elm, x, y)
+            }).bind(this))
+        }
+
+        genElmGrid() {
+            this.#elmGrid = Array(this.#width).fill(undefined).map(t => new Array(this.#height));
+            this.gridElm = document.createElement('div')
+            for (let x = 0; x < this.#width; x++) {
+                var row = document.createElement('tr')
+                for (let y = 0; y < this.#height; y++) {
+                    this.#elmGrid[x][y] = document.createElement('td')
+                    this.#elmGrid[x][y].innerText = this.#grid[x][y]
+                    this.#elmGrid[x][y].setAttribute('x', x)
+                    this.#elmGrid[x][y].setAttribute('y', y)
+                    row.appendChild(this.#elmGrid[x][y])
+                    this.#provideFunctionality(this.#elmGrid[x][y], x, y)
+                }
+                this.gridElm.appendChild(row)
+            }
+            return this.gridElm
         }
 
         /**
@@ -84,6 +195,10 @@
     }
 
     await game.init()
-    new game()
+    var ssGame = new game()
+
+    if (!document.readyState == 'compleate') await new Promise(r => document.addEventListener('DOMContentLoaded', r));
+
+    document.body.appendChild(ssGame.gridElm)
 
 })()

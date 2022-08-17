08214ba97c9f84e091ee0c34b939783887b2e290
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-08-17 13:30:47 +1000

    Update

--- srcipts/js/index.js
+++ srcipts/js/index.js
@@ -6,6 +6,7 @@
         #height = 10
 
         #grid = []
+        #elmGrid
 
         #number = 0
         #factors = []
@@ -23,7 +24,53 @@
         #gengrid() {
             this.#grid = Array(this.#width).fill(undefined).map(t => new Array(this.#height));
 
-            console.log(this.#number, this.#factors)
+            var unfilledSquares = this.#width * this.#height
+
+            this.#factors.forEach(t => {
+                var poz = this.getRandomPosition(true)
+                this.#grid[poz.x][poz.y] = t
+                unfilledSquares--
+            })
+
+            while (unfilledSquares--) {
+                var poz = this.getRandomPosition(true)
+                this.#grid[poz.x][poz.y] = this.#factors[Math.floor(Math.random() * this.#factors.length)]
+            }
+
+            console.log(this.#grid)
+        }
+
+        genGrid() {
+            for (let x = 0; x < array.length; x++) {
+                for (let y = 0; y < array.length; y++) {
+                }
+            }
+        }
+
+        /**
+         * 
+         * @param {boolean} filtered 
+         * @returns {{x:number,y:number}}
+         */
+        getRandomPosition(filtered) {
+            var x = Math.floor(Math.random() * this.#height),
+                y = Math.floor(Math.random() * this.#width);
+            if (!filtered) {
+                return {
+                    x: x,
+                    y: y
+                }
+            } else {
+                while (this.#grid[x][y]) {
+                    x = Math.floor(Math.random() * this.#height)
+                    y = Math.floor(Math.random() * this.#width)
+                }
+                return {
+                    x: x,
+                    y: y
+                }
+            }
+
         }
 
         static #numbs = [{ n: 0, f: [0] }]
